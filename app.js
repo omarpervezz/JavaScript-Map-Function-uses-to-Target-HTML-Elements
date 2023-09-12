b18ffb3cd51cef7268cec0661bbd21c5b5b37db2
Omar Pervez <freelancertusar5182@gmail.com>
2023-09-12 13:34:20 +0600

    added the Map function code

---
+++ app.js
@@ -0,0 +1,66 @@
+function setElements(ids){
+   const element = new Map();
+   ids.forEach((id) => {
+    element.set(id, document.getElementById(`${id}`));
+    element.get(id).style = 'font-size:20px;margin:20px 0;color:white;background:black;padding:20px; opacity:0.7'
+   });
+
+   return element;
+}
+
+const elementId = [
+    "id-1",
+    "id-2",
+    "id-3",
+    "id-4",
+    "id-5",
+    "id-6",
+    "id-7",
+    "id-8",
+    "id-9",
+    "id-10",
+]
+
+// call the function to get the elements array as parameters
+const elements1 = setElements(elementId);
+
+
+elements1.get('id-1').addEventListener('click', function(){
+    alert(elements1.get('id-1').innerText)
+})
+
+elements1.get('id-2').addEventListener('click', function(){
+    alert(elements1.get('id-2').innerText);
+});
+
+elements1.get('id-3').addEventListener('click', function(){
+    alert(elements1.get('id-3').innerText);
+})
+
+elements1.get('id-4').addEventListener('click', function(){
+    alert(elements1.get('id-4').innerText);
+})
+
+elements1.get('id-5').addEventListener('click', function(){
+    alert(elements1.get('id-5').innerText);
+})
+
+elements1.get('id-6').addEventListener('click', function(){
+    alert(elements1.get('id-6').innerText);
+})
+
+elements1.get('id-7').addEventListener('click', function(){
+    alert(elements1.get('id-7').innerText);
+});
+
+elements1.get('id-8').addEventListener('click', function(){
+    alert(elements1.get('id-8').innerText)
+});
+
+elements1.get('id-9').addEventListener('click', function(){
+    alert(elements1.get('id-9').innerText)
+});
+
+elements1.get('id-10').addEventListener('click', function(){
+    alert(elements1.get('id-10').innerText)
+})
